75c7d3635fecd441a92ad5becb5ec3c5da900421
heyangzhou <he.yangzhou@redclouds.com>
2021-07-28 21:36:11 +0800

    fix: gauge config fix

--- src/config/charts/gauge.js
+++ src/config/charts/gauge.js
@@ -2,23 +2,65 @@ export default {
   type: 'chart',
   settings: {
     type: 'gauge',
+    seriesMap: {
+      有进展: {
+        anchor: false,
+        pointer: false,
+        axisTick: false,
+        splitNumber: 5,
+        max: 200,
+        progress: {
+          show: true,
+          overlap: true,
+          roundCap: true,
+          width: 30,
+        },
+        axisLabel: {
+          distance: 35,
+        },
+        splitLine: {
+          length: 5,
+          distance: 2,
+        },
+        axisLine: {
+          roundCap: true,
+          lineStyle: {
+            width: 30,
+          },
+        },
+        title: {
+          fontSize: 14,
+          width: 180,
+          height: 36,
+          borderColor: '#5470c6',
+          borderWidth: 1,
+          borderRadius: 18,
+          color: '#5470c6',
+          offsetCenter: ['0', '80%'],
+        },
+        detail: {
+          formatter: '{value}',
+          offsetCenter: ['0', '0'],
+        },
+      },
+    },
   },
   datacon: {
     type: 'raw',
     connectId: '',
     data: {
-      columns: ['访问用户'],
+      columns: ['任务状态', '完成数量'],
       rows: [
-        { 访问用户: 1523 },
+        { 任务状态: '有进展', 完成数量: 50 },
       ],
     },
     getUrl: '',
     interval: 2,
   },
   generated: {
-    columns: ['访问用户'],
+    columns: ['完成率'],
     rows: [
-      { 访问用户: 1523 },
+      { 完成率: 20 },
     ],
   },
 };
